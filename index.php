ba5459f6c8d64f7c7b87213055767d1bb760938b
Brandon <btzippe@gmail.com>
2020-12-14 12:04:07 -0600

    fixing home head.php and adding brand panel control

--- index.php
+++ index.php
@@ -7,37 +7,14 @@
     <!-- Header -->
 
     <header id="top" class="header-home">
-      <div class="brand-panel">
-        <a href="#top" class="brand js-target-scroll">
-          <img src="img/slider/BSHAH-black.png"  alt="" height="30">
-        </a>
-        <div class="brand-name">Shah</div>
-        <div class="slide-number">
-          <span class="current-number text-primary">0<span class="count">1</span></span>
-          <sup><span class="delimiter">/</span> 0<span class="total-count"></span></sup>
-        </div>
-      </div>
-      <a href="tel:+14178209340"><div class="header-phone">+1 (417) 820-9340</div></a>
-      <div class="vertical-panel"></div>
-      <div class="vertical-panel-content">
-        <div class="vertical-panel-info">
-            <div class="vertical-panel-title">DR.Shah</div>
-            <div class="line"></div>
-         </div>
-        <ul class="social-list">
-          <li><a href="" class="fa fa-instagram"></a></li>
-          <li><a href="" class="fa fa-twitter"></a></li>
-          <li><a href="" class="fa fa-linkedin"></a></li>
-          <li><a href="" class="fa fa-facebook"></a></li>
-         </ul>
-      </div>
-
-      <!-- Navigation Desctop -->
-      <?php include ('php/global/desktopNav.php');?>
-
-      <!-- Navigation Mobile -->
-      <?php include ('php/global/mobileNav.php');?>
-
+      <?php
+        //Brand Panel
+        include ('php/global/brandPanel.php');
+        //<!-- Navigation Desctop -->
+        include ('php/global/desktopNav.php');
+        //<!-- Navigation Mobile -->
+        include ('php/global/mobileNav.php');
+      ?>
     </header>
 
   <!-- Layout -->
--- php/home/head.php
+++ php/home/head.php
@@ -1,8 +1,19 @@
 <?php echo '
   <head>
+  <meta  charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Dr. Bharat Shah</title>
+
+  <!-- Favicons -->
+  <link rel="shortcut icon" href="favicon.png">
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
+
+  <!-- Styles -->
+  <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet" media="screen">
+  <link rel="stylesheet" type="text/css" href="css/base.css" />
+  </head>
 ';?>
-<?php include ('/global/globalMeta.php');?>
-<?php include ('/global/favicons.php');?>
-<?php include ('/global/styles.php');?>
-<?php echo '</head>';?>
